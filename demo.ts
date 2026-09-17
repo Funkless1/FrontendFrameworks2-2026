7965fdf94b7a52ffece275065dea592c7a64c830
Nick Randall <njrandall@me.com>
2026-09-17 14:11:57 -0300

    added fireweapon method and weapons enum

--- demo.ts
+++ demo.ts
@@ -9,4 +9,13 @@ export class Demo {
     fire(): void {
         console.log('Bang!');
     }
+
+    //to parse ${weapon} properly, use backtcks ``
+    fireweapon(weapon: Weapons): void {
+        console.log(`Fire the ${weapon}`);
+    }
+
+
 }
+
+export enum Weapons {Cannon, NerfGun, Raygun, OrbitalSatellite}
