c6114a95803442d4f2e99c21896e370be3b5ffcc
Nick Randall <njrandall@me.com>
2026-09-17 15:00:52 -0300

    added fireweaponToString method and console output

--- demo.js
+++ demo.js
@@ -11,13 +11,24 @@ export class Demo {
     fireweapon(weapon) {
         console.log(`Fire the ${weapon}`);
     }
+    //lookup the name of the enumeration based on the value
+    fireweaponToString(weapon) {
+        return Weapons[weapon];
+    }
 }
+//enumerations auto increment from previous value
+//you can set canno to 10, then the nerfgun would be 11 and so on
 export var Weapons;
 (function (Weapons) {
-    Weapons[Weapons["Cannon"] = 0] = "Cannon";
-    Weapons[Weapons["NerfGun"] = 1] = "NerfGun";
-    Weapons[Weapons["Raygun"] = 2] = "Raygun";
-    Weapons[Weapons["OrbitalSatellite"] = 3] = "OrbitalSatellite";
+    Weapons[Weapons["Cannon"] = 10] = "Cannon";
+    Weapons[Weapons["NerfGun"] = 11] = "NerfGun";
+    Weapons[Weapons["Raygun"] = 12] = "Raygun";
+    Weapons[Weapons["OrbitalSatellite"] = 13] = "OrbitalSatellite";
 })(Weapons || (Weapons = {}));
 let myFirstWeapon = new Demo(`Dob`);
 console.log(`I'm a demo object named ${myFirstWeapon.name}`);
+myFirstWeapon.fire();
+myFirstWeapon.fireweapon(Weapons.OrbitalSatellite);
+let weaponUsed;
+weaponUsed = myFirstWeapon.fireweaponToString(Weapons.OrbitalSatellite);
+console.log(`The weapon with the number ${Weapons.OrbitalSatellite} is the ${weaponUsed}`);
--- demo.ts
+++ demo.ts
@@ -12,10 +12,24 @@ export class Demo {
     fireweapon(weapon: Weapons): void {
         console.log(`Fire the ${weapon}`);
     }
+
+    //lookup the name of the enumeration based on the value
+    fireweaponToString(weapon: Weapons): string {
+        return Weapons[weapon];
+    }
 }
 
-export enum Weapons {Cannon, NerfGun, Raygun, OrbitalSatellite}
+//enumerations auto increment from previous value
+//you can set canno to 10, then the nerfgun would be 11 and so on
+export enum Weapons {Cannon = 10, NerfGun, Raygun, OrbitalSatellite}
 
 let myFirstWeapon = new Demo(`Dob`);
 
 console.log(`I'm a demo object named ${myFirstWeapon.name}`);
+
+myFirstWeapon.fire();
+myFirstWeapon.fireweapon(Weapons.OrbitalSatellite);
+
+let weaponUsed: string;
+weaponUsed = myFirstWeapon.fireweaponToString(Weapons.OrbitalSatellite);
+console.log(`The weapon with the number ${Weapons.OrbitalSatellite} is the ${weaponUsed}`);
